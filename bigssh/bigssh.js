1bc4cb95c273cec589aed142c9a315c24752cb80
Jacques W <jacquesn4444@gmail.com>
2016-04-24 23:09:08 +0200

    checkpoint

---
+++ bigssh/bigssh.js
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2013, 2015 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Original and good work by IBM
+ * "Big Nodes" mods by Jacques W
+ *
+ * /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+ *
+ *  Big Nodes principles:
+ *
+ *  #1 can handle big data
+ *  #2 send start/end messages
+ *  #3 tell what they are doing
+ *
+ *  Any issues? https://github.com/Jacques44
+ *
+ * /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+ *
+ **/
+
+module.exports = function(RED) {
+
+    "use strict";
+
+    var biglib = require('node-red-biglib');
+
+    function BigSSH(config) {
+
+      RED.nodes.createNode(this, config);
+
+      // new instance of biglib for this configuration
+      var bignode = new biglib({ config: config, node: this, status: 'filesize', generator: 'remote' });
+
+      // biglib changes the configuration to add some properties
+      config = bignode.config();
+
+      this.on('input', bignode.main.bind(bignode));
+    }
+
+    RED.nodes.registerType("bigssh",BigSSH);
+}
